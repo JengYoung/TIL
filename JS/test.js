1ec4493b0395bfbfb3c3b24f8f5cd5264646be4c
Jengyoung <young_pallete@naver.com>
2021-06-13 21:28:08 +0900

    콜백함수__210613

--- JS/test.js
+++ JS/test.js
@@ -33,3 +33,23 @@ var sub = function(x, y) {
 }
 
 console.log(sub(2,5))
+
+
+function repeat(n, f) {
+    for (var i = 0; i < n; i++) {
+        f(i)
+    }
+}
+
+var logAll = function(i) {
+    console.log(i);
+}
+
+// 반복 호출할 함수를 인수로 전달
+repeat(5, logAll); // 0 1 2 3 4
+
+var logOdds = function (i) {
+    if (i % 2) console.log(i);
+};
+console.log("---")
+repeat(5, logOdds);
