d2145bf65919f830c7064ad3e452ad9bc66948e4
Jengyoung <young_pallete@naver.com>
2021-06-13 19:22:36 +0900

    함수__210613

--- JS/test.js
+++ JS/test.js
@@ -18,3 +18,18 @@ function makeObj () {
 }
 const whatIsThis = makeObj();
 console.log(whatIsThis);
+
+
+console.dir(add)
+console.dir(sub)
+console.log(add(2,5))
+
+function add(x, y) {
+    return x + y;
+};
+
+var sub = function(x, y) {
+    return x - y; 
+}
+
+console.log(sub(2,5))
