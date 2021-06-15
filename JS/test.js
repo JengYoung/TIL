4253c254c78a76968ed585b34058940a1a956fc5
Jengyoung <young_pallete@naver.com>
2021-06-15 14:13:33 +0900

    일급 객체(함수)__210615

--- JS/test.js
+++ JS/test.js
@@ -1,85 +1,34 @@
-// const person = new Object();
-
-// person.name = 'Hwang';
-// person.greeting = function() {
-//     console.log(`Hi! ${this.name}`)
-// };
-
-// console.log(person);
-// person.greeting();
-
-
-// function Circle(radius) {
-//     this.radius = radius;
-//     this.getDiameter = function() {
-//         return 2 * this.radius;
-//     };
-
-//     return { name: 'jaeyoung'};
-// };
-
-// const circle = new Circle(1);
-
-// console.log(circle)
-
-
-// function foo() {}
-
-
-// foo.prop = 10;
-
-// foo.method = function() {
-//     console.log(this.prop);
-// }
-// foo.method() // 10
-
-// function foo () {}
-// const bar = function() {};
-
-// const baz = {
-//     x: function () {}
-// };
-
-// console.log(new foo())
-// console.log(new bar())
-// console.log(new baz.x());
-
-// const arrow = () => {}; 
-
-// // new arrow()// Type Error! (non-constructor)
-
-// const obj = {
-//     x() {}
-// };
-
-// // new obj.x(); // Type Error! (non-constructor)
-
-// function add (x,y) {
-//     return x + y;
-// };
-
-// let inst = new add();
-// console.log(inst)
-
-function Circle(radius) {
-    // if not call by using new operator -> undefined
-    if (!new.target) {
-        return new Circle(radius);
-    }
-    this.radius = radius;
-    this.getDiameter = function () {
-        return 2 * this.radius;
-    };
+function square(num) {
+    return num * num
 }
+console.dir(square) // 함수 객체 내부 확인
+console.log(Object.getOwnPropertyDescriptors(square))
 
-const circle = Circle(5);
-console.log(circle.getDiameter())
+console.log(Object.getOwnPropertyDescriptor(square, '__proto__')) // undefined
 
-const str = String(123);
-console.log(str, typeof str);
+console.log(Object.getOwnPropertyDescriptor(Object.prototype, '__proto__')) // undefined
 
-const num = Number('123');
-console.log(num, typeof num);
 
-const bool = Boolean('true');
-console.log(bool, typeof bool);
+function multiply(x, y) {
+    
+    const iterator = arguments[Symbol.iterator]();
+
+    // next 메서드 호출 -> iterable 객체 arguments를 순회
+    console.log(iterator.next());
+    console.log(iterator.next());
+    console.log(iterator.next());
+    console.log(iterator.next());
+
+    return x * y;
+};
+
+multiply(1,2,3)
+
+
+
+const obj =  { a: 1 };
+
+console.log(obj.__proto__ === Object.prototype); // true
+
+console.log(obj.hasOwnProperty('a'));
+console.log(obj.hasOwnProperty('__proto__'));
