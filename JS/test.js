7366017baadf745962ccd6f09eb9a423022f5736
Jengyoung <young_pallete@naver.com>
2021-06-15 15:05:38 +0900

    전역 변수 모듈 패턴 설명 추가

--- JS/test.js
+++ JS/test.js
@@ -30,5 +30,5 @@ const obj =  { a: 1 };
 
 console.log(obj.__proto__ === Object.prototype); // true
 
-console.log(obj.hasOwnProperty('a'));
-console.log(obj.hasOwnProperty('__proto__'));
+console.log(obj.hasOwnProperty('a')); // true
+console.log(obj.hasOwnProperty('__proto__')); // false
