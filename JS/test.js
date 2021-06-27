cc91c66dbaf2156c2b65e4a1160f7ea04bdc7d3c
Jengyoung <young_pallete@naver.com>
2021-06-27 23:22:16 +0900

    클로저__210627

--- JS/test.js
+++ JS/test.js
@@ -1,2 +1,21 @@
-var x = 1;
-console.log(global.x)
+const counter = (function () {
+    let counter = 0;
+    return function (predicate) {
+        counter = predicate(counter);
+        return counter;
+    }
+}())
+
+function increase(n) {
+    return ++n;
+}
+
+function decrease(n) {
+    return --n;
+}
+
+console.log(counter(increase));
+console.log(counter(increase));
+console.log(counter(decrease));
+console.log(counter(decrease));
+console.log(counter(decrease));
