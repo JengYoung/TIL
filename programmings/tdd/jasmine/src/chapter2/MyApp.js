b4a467cbfa6bb5529301c10655522784613633c4
JengYoung <young_pallete@naver.com>
2022-12-12 16:51:13 +0900

    :memo: [TDD] 의존성 주입 활용

---
+++ programmings/tdd/jasmine/src/chapter2/MyApp.js
@@ -0,0 +1,30 @@
+import DIContainer from "./DIContainer";
+
+const MyApp = {};
+
+MyApp.diContainer = new DIContainer();
+MyApp.diContainer.register(
+  'Service',
+  [],
+  function() {
+    return new ConferenceWebSvc();
+  }
+);
+
+MyApp.diContainer.register(
+  'Messenger',
+  [],
+  function() {
+    return new Messenger();
+  }
+)
+
+MyApp.diContainer.register(
+  'AttendeeFactory',
+  ['Service', 'Messenger'], // Attendee는 service 및 messenger에 의존한다.
+  function (service, messenger) {
+    return function(attendeeId) {
+      return new Attendee(service, messenger, attendeeId);
+    }
+  }
+)
